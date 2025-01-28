65e688d759eb2b348cab8bb39028b60f75158260
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-01-27 23:37:37 -0400

    include `libGL.so`
    fixes opengl not working

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -75,6 +75,7 @@ chmod +x ./lib4bin
 xvfb-run -a -- ./lib4bin -p -v -e -s -k \
 	/usr/bin/citron* \
 	/usr/lib/libGLX* \
+	/usr/lib/libGL.so* \
 	/usr/lib/libEGL* \
 	/usr/lib/dri/* \
 	/usr/lib/libvulkan* \
