c87faf405bc13310e336e1f3e9b57c32cfbb6cf0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-03-13 22:34:59 +0000

    build citron manually instead of relying on the aur package (#13)
    this will make it easier to add nightly and arm builds next

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 export APPIMAGE_EXTRACT_AND_RUN=1
 export ARCH="$(uname -m)"
+
+REPO="https://git.citron-emu.org/Citron/Citron.git"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 URUNTIME=$(wget --retry-connrefused --tries=30 \
 	https://api.github.com/repos/VHSgunzo/uruntime/releases -O - \
@@ -13,39 +15,63 @@ ICON="https://git.citron-emu.org/Citron/Citron/raw/branch/master/dist/citron.svg
 if [ "$1" = 'v3' ]; then
 	echo "Making x86-64-v3 build of citron"
 	ARCH="${ARCH}_v3"
+	ARCH_FLAGS="-march=x86-64-v3 -O3"
+else
+	echo "Making x86-64-v3 generic of citron"
+	ARCH_FLAGS="-march=x86-64 -mtune=generic -O3"
 fi
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
 # BUILD CITRON
-if [ ! -d ./citron ]; then
-	git clone https://aur.archlinux.org/citron.git citron
-fi
-cd ./citron
+git clone https://git.citron-emu.org/Citron/Citron.git ./citron
 
-if [ "$1" = 'v3' ]; then
-	sed -i 's/-march=[^"]*/-march=x86-64-v3/g' ./PKGBUILD
-	sudo sed -i 's/-march=x86-64 /-march=x86-64-v3 /' /etc/makepkg.conf # Do I need to do this as well?
-	cat /etc/makepkg.conf
-else
-	sed -i 's/-march=[^"]*/-march=x86-64/g' ./PKGBUILD
-fi
+( cd ./citron
+	if [ "$DEVEL" = 'true' ]; then
+		CITRON_TAG="$(git rev-parse --short HEAD)"
+		echo "Making nightly \"$CITRON_TAG\" build"
+		VERSION="$CITRON_TAG"
+	else
+		CITRON_TAG=$(git describe --tags)
+		echo "Making stable \"$CITRON_TAG\" build"
+		git checkout "$CITRON_TAG"
+		VERSION="$(echo "$CITRON_TAG" | awk -F'-' '{print $1}')"
+	fi
+	git submodule update --init --recursive
 
-# This library is massive and makes the AppImage +220 Mib
-# Seems to have very few  uses so we will build without it
-sed -i "s/'qt6-webengine'//" ./PKGBUILD
-sed -i 's/-DCITRON_USE_QT_WEB_ENGINE=ON/-DCITRON_USE_QT_WEB_ENGINE=OFF/' ./PKGBUILD
+	#Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
+	find src -type f -name '*.cpp' -exec sed -i 's/boost::asio::io_service/boost::asio::io_context/g' {} \;
 
-if ! grep -q -- '-O3' ./PKGBUILD; then
-	sed -i 's/-march=/-O3 -march=/g' ./PKGBUILD
-fi
-cat ./PKGBUILD
-
-makepkg -f
-sudo pacman --noconfirm -U *.pkg.tar.*
-ls .
-export VERSION="$(awk -F'=' '/pkgver=/{print $2; exit}' ./PKGBUILD)"
-echo "$VERSION" > ~/version
-cd ..
+	mkdir build
+	cd build
+	cmake .. -GNinja \
+		-DCITRON_USE_BUNDLED_VCPKG=OFF \
+		-DCITRON_USE_BUNDLED_QT=OFF \
+		-DUSE_SYSTEM_QT=ON \
+		-DCITRON_USE_BUNDLED_FFMPEG=OFF \
+		-DCITRON_USE_BUNDLED_SDL2=ON \
+		-DCITRON_USE_EXTERNAL_SDL2=OFF \
+		-DCITRON_TESTS=OFF \
+		-DCITRON_CHECK_SUBMODULES=OFF \
+		-DCITRON_USE_LLVM_DEMANGLE=OFF \
+		-DCITRON_ENABLE_LTO=ON \
+		-DCITRON_USE_QT_MULTIMEDIA=ON \
+		-DCITRON_USE_QT_WEB_ENGINE=OFF \
+		-DENABLE_QT_TRANSLATION=ON \
+		-DUSE_DISCORD_PRESENCE=OFF \
+		-DBUNDLE_SPEEX=ON \
+		-DCITRON_USE_FASTER_LD=OFF \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_CXX_FLAGS="$ARCH_FLAGS -Wno-error" \
+		-DCMAKE_C_FLAGS="$ARCH_FLAGS" \
+		-DCITRON_ENABLE_PGO_OPTIMIZE=ON \
+		-DCMAKE_SYSTEM_PROCESSOR="$(uname -m)" \
+		-DCMAKE_BUILD_TYPE=Release
+	ninja
+	sudo ninja install
+	echo "$VERSION" > ~/version
+)
+rm -rf ./citron
+VERSION="$(cat ~/version)"
 
 # NOW MAKE APPIMAGE
 mkdir ./AppDir
